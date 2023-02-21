258b392d7eb2aa306a40563aa2633d3d48abfc14
anton biruk <anton.biruk@solidone.net>
2023-02-21 13:25:12 +0200

    style refactoring

--- src/Components/HourlyTemperature/HourlyTemperature.tsx
+++ src/Components/HourlyTemperature/HourlyTemperature.tsx
@@ -9,18 +9,21 @@ const HourlyTemperature: React.FC<any> = ({ hourlyDeatails }) => {
         <div style={{ display: "flex", height: "50px", alignItems: "flex-end", gap: "1px" }}>
             {
                 hourlyDeatails?.map((i: HourlyTemperatureType) => (
-                        <div style={{ 
-                        display: "flex",
-                        justifyContent: "center",
-                        alignItems: "center",
-                        border: "1px solid #c3c3c3", 
-                        background: "#dedede",
-                        width: "60px",
-                        height: "30px",
-                        marginBottom: `${i.temperature}px`
-                    }}>
-                        {i.temperature}°C
-                    </div>
+                        <div style={{ textAlign: "center" }}>
+                            {i.time}:00
+                            <div style={{ 
+                                display: "flex",
+                                justifyContent: "center",
+                                alignItems: "center",
+                                border: "1px solid #c3c3c3", 
+                                background: "#dedede",
+                                width: "60px",
+                                height: "30px",
+                                marginBottom: `${i.temperature}px`
+                            }}>
+                                {i.temperature}°C
+                            </div>
+                        </div>
                 ))
             }
         </div>
